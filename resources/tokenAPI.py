042680069840a9e285ec9c387176e439a33aebcc
fabiank0 <kuefferfabian@gmail.com>
2021-11-28 13:27:43 +0100

    - PUT api route for the client to get a nonce - POST to token to get a token requires now a valid nonce and its digitally signed hash by the user

--- resources/tokenAPI.py
+++ resources/tokenAPI.py
@@ -1,19 +1,33 @@
 from flask import Response, request
 from flask_restful import Resource
-from services import createToken
+from services import createToken, createNonce
 import json
 
 
 class TokenAPI(Resource):
-    def post(self):
+    def post(self) -> Response:
         """
-        :return: string: token
+        Returns a new token if auth successful
+        :return: Response
         """
-        signedValue = request.get_json().get('signedValue')
-        value = request.get_json().get('value')
+        signedNonce = request.get_json().get('signedNonce')
+        nonce = request.get_json().get('nonce')
+        address = request.get_json().get('address')
 
-        token = createToken(value, signedValue)
+        token = createToken(nonce, signedNonce, address)
         if token:
             return Response(json.dumps({"token": token}), mimetype='application/json', status=200)
+        else:
+            return Response(mimetype='application/json', status=403)
+
+    def put(self) -> Response:
+        """
+        Creates and returns a new nonce
+        :return: Response
+        """
+        address = request.get_json().get('address')
+        nonce = createNonce(address)
+        if nonce:
+            return Response(json.dumps({"nonce": nonce}), mimetype='application/json', status=200)
         else:
             return Response(mimetype='application/json', status=403)
