ecd3655bc26b868f217bd378fc887bec43c57e93
fabiank0 <kuefferfabian@gmail.com>
2022-02-07 19:07:17 +0100

    close the opened file

--- openapi_server/contract/w3.py
+++ openapi_server/contract/w3.py
@@ -4,6 +4,8 @@ import config
 
 url = config.WEB3_CONFIG['URL']
 w3 = Web3(Web3.HTTPProvider(url))
-file = open(config.SC_ABI_PATH)
-contractJSON = json.load(file)
+
+with open(config.SC_ABI_PATH) as f:
+    contractJSON = json.load(f)
+
 contract = w3.eth.contract(address=contractJSON['networks']['5777']['address'], abi=contractJSON['abi'])
